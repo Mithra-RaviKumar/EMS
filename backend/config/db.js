843469e3a573ae93275e39b8f57eeefb96a8f31a
Mithra-RaviKumar <mithraravi528@gmail.com>
2025-08-23 12:33:04 +0530

    backend updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ems");
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(" MongoDB Connected");
   } catch (err) {
-    console.error("❌ MongoDB Connection Failed", err.message);
+    console.error(" MongoDB Connection Failed", err.message);
     process.exit(1);
   }
 };
